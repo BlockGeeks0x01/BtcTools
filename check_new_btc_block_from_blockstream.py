2d60dd04daf914117fbe2a9be8dd284faacece73
BlockGeeks <blockgeeks@163.com>
2023-03-20 17:52:31 +0800

    add log

--- check_new_btc_block_from_blockstream.py
+++ check_new_btc_block_from_blockstream.py
@@ -26,4 +26,10 @@ if __name__ == '__main__':
         if current_block_hash != best_btc_block_hash:
             best_btc_block_hash = current_block_hash
             logging.info(f">>>>>>>>>>>>> find new btc block: {best_btc_block_hash}")
+            block_header = session.get(f"{api_end_point}/block/{best_btc_block_hash}/header").content.decode('utf-8')
+            logging.info("finish get block header")
+            block_tx_list = session.get(f"{api_end_point}/block/{best_btc_block_hash}/txids").json()
+            logging.info("finish get block tx list")
+            coinbase_tx = session.get(f"{api_end_point}/tx/{block_tx_list[0]}/hex").content.decode('utf-8')
+            logging.info("finish get coinbase tx")
         time.sleep(0.5)
